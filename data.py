318739f15fea6f64d9002f3fca767008c8b4c4e7
Juan Esteban Tobón Duque <juanestd1013@hotmail.com>
2024-09-30 12:05:22 -0500

    .

--- data.py
+++ data.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
 def cargar_datos_cxc():
-    archivo_excel = 'C:\\Users\\juane\\Downloads\\CXC 2023-2024.xlsx'
+    archivo_excel = 'D:\\J Y P\\CXC 2020\\CXC\\CXC 2023-2024.xlsx'
     df = pd.read_excel(archivo_excel, sheet_name='CXC', header=5)
     return df
 
 def cargar_datos_rem_nal():
-    archivo_excel = 'C:\\Users\\juane\\Downloads\\CXC 2023-2024.xlsx'
+    archivo_excel = 'D:\\J Y P\\CXC 2020\\CXC\\CXC 2023-2024.xlsx'
     df = pd.read_excel(archivo_excel, sheet_name='REM NAL', header=0)
     return df
 
 def cargar_datos_facturas_nal():
-    archivo_excel = 'C:\\Users\\juane\\Downloads\\CXC 2023-2024.xlsx'
+    archivo_excel = 'D:\\J Y P\\CXC 2020\\CXC\\CXC 2023-2024.xlsx'
     df = pd.read_excel(archivo_excel, sheet_name='FACTURAS NAL', header=5)
     return df
 
 def cargar_datos_creditos_reclamos():
-    archivo_excel = 'C:\\Users\\juane\\Downloads\\CXC 2023-2024.xlsx'
+    archivo_excel = 'D:\\J Y P\\CXC 2020\\CXC\\CXC 2023-2024.xlsx'
     df = pd.read_excel(archivo_excel, sheet_name='Creditos-Reclamos', header=5)
     return df
